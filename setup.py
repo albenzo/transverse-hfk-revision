1ac7ca438d639ba6ad80e727d8b4986c2ee3a91d
Lucas Meyers <lmeye22@lsu.edu>
2019-02-23 17:19:17 -0600

    Adding skeleton files setup.py and __init__.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup, Extension
 
 _transverseHFK_module = Extension('transverseHFK._transverseHFK',
-                                  sources = ['_transverse_module'],
+                                  sources = ['_transverseHFK_module'],
                                   include_dirs = ['.'])
 
 setup(name = '_transverseHFK',
       version = '1.0',
       description = 'Computes transverse knot invariants',
       packages = ['transverseHFK'],
-      ext_modules = [_transverse_module])
+      ext_modules = [_transverseHFK_module])
--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,26 @@
+import transverseHFK._transverseHFK as _tHFK
+from Tkinter import *
+
 __all__ = ['transverseHFK', 'Tk_transverseHFK']
 
 class transverseHFK:
     """
     """
+
+    def __init__(self, Xs, Os, name=None):
+        """
+        """
+        self.Xs = Xs
+        self.Os = Os
+        self.name = name
+        self.verbose = false
+        self.timeout = None
+
+    def null_homologous_D0Q(self,state):
+        return 
+
+    def null_homologous_D1Q(self,state):
+        return
 
 class Tk_transverseHFK:
     """
